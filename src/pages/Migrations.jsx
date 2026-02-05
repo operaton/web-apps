f2bf439a0b532e7f9018c53a998235f6e969a12e
Paul <paul.hempel@lambdaschmiede.com>
2026-02-05 16:13:57 +0100

    add moddle library to get activities for mapping

--- src/pages/Migrations.jsx
+++ src/pages/Migrations.jsx
@@ -1,5 +1,5 @@
 import { signal } from "@preact/signals";
-import { useContext } from "preact/hooks";
+import { useContext, useEffect } from "preact/hooks";
 import engine_rest, {
   RequestState,
   RESPONSE_STATE,
@@ -8,6 +8,7 @@ import { AppState } from "../state.js";
 import { createContext } from "preact";
 import ReactBpmn from "react-bpmn";
 import BpmnModdle from "bpmn-moddle";
+import { diff } from "bpmn-js-differ";
 import { useLocation, useRoute } from "preact-iso/router";
 
 const create_mirgation_state = () => {
@@ -77,14 +78,6 @@ const ProcessSelection = () => {
       state,
       migration_state.source.value,
     );
-    // void engine_rest.task
-    //   .get_task_process_definitions(state, migration_state.source.value)
-    //   .then(console.log);
-    // void engine_rest.process_definition.activity_instance_statistics(
-    //   state,
-    //   migration_state.source.value,
-    // );
-    // .then(() => console.log(state.api.process.definition.activity_instance_statistics.value))
   }
   if (query.target) {
     migration_state.target.value = query.target;
@@ -103,96 +96,134 @@ const ProcessSelection = () => {
     );
   }
 
-  return (
-    <div>
-      <label>Source Process Definition</label>
-      <br />
-      <select
-        onChange={(e) => {
-          // @ts-ignore
-          migration_state.source.value = e.target.value;
-          add_query_params("source", migration_state.source.value);
-          engine_rest.process_definition.diagram(
-            state,
-            migration_state.source.value,
-            migration_state.source_diagram,
-          );
-          engine_rest.process_instance.by_defintion_id(
-            state,
-            migration_state.source.value,
-          );
-        }}
-      >
-        <option disabled selected>
-          -- select source ---
-        </option>
-        <RequestState
-          state={state}
-          signal={list}
-          on_success={() =>
-            list.value.data.map(({ id, definition }) => (
-              <option
-                key={id}
-                disabled={migration_state.target.value == id}
-                selected={migration_state.source.value == id}
-                value={id}
-              >
-                {definition.name} – v{definition.version}
-              </option>
-            ))
-          }
-        />
-      </select>
-      <br />
-      <br />
+  // useEffect(() => {
+  //   if (
+  //     migration_state.source_diagram.value !== null &&
+  //     migration_state.source_diagram.value.data !== null
+  //   ) {
+  //     const bpmnModdle = new BpmnModdle();
 
-      <label>Target Process Definition</label>
-      <br />
-      <select
-        onChange={(e) => {
-          migration_state.target.value = e.target.value;
-          add_query_params("target", migration_state.target.value);
-          engine_rest.process_definition.diagram(
-            state,
-            migration_state.target.value,
-            migration_state.target_diagram,
-          );
-        }}
-      >
-        <option disabled selected>
-          -- select target ---
-        </option>
-        <RequestState
-          state={state}
-          signal={list}
-          on_success={() =>
-            list.value.data.map(({ id, definition }) => (
-              <option
-                key={id}
-                disabled={migration_state.source.value == id}
-                selected={migration_state.target.value == id}
-                value={id}
-              >
-                {definition.name} – v{definition.version}
+  //     console.log("xml", migration_state.source_diagram.value.data?.bpmn20Xml);
+
+  //     // const { rootElement: definitionsA } = await;
+  //     bpmnModdle
+  //       .fromXML(migration_state.source_diagram.value.data?.bpmn20Xml)
+  //       .then(({ rootElement: definitions }) =>
+  //         console.log("diff moddle", definitions),
+  //       );
+  //   }
+
+  //   console.log(
+  //     "diff",
+  //     diff(
+  //       migration_state.source_diagram.value.data?.bpmn20Xml,
+  //       migration_state.target_diagram.value.data?.bpmn20Xml,
+  //     ),
+  //   );
+  // });
+
+  return (
+    <div id="migration">
+      <h1 class="screen-hidden">Migrations</h1>
+      <section>
+        <h2>Process Defintion Selection</h2>
+        <div class="row">
+          <div>
+            <label>Source Process Definition</label>
+            <br />
+            <select
+              onChange={(e) => {
+                // @ts-ignore
+                migration_state.source.value = e.target.value;
+                add_query_params("source", migration_state.source.value);
+                engine_rest.process_definition.diagram(
+                  state,
+                  migration_state.source.value,
+                  migration_state.source_diagram,
+                );
+                engine_rest.process_instance.by_defintion_id(
+                  state,
+                  migration_state.source.value,
+                );
+              }}
+            >
+              <option disabled selected>
+                -- Select source ---
               </option>
-            ))
-          }
-        />
-      </select>
+              <RequestState
+                state={state}
+                signal={list}
+                on_success={() =>
+                  list.value.data.map(({ id, definition }) => (
+                    <option
+                      key={id}
+                      disabled={migration_state.target.value == id}
+                      selected={migration_state.source.value == id}
+                      value={id}
+                    >
+                      {definition.name} – v{definition.version}
+                    </option>
+                  ))
+                }
+              />
+            </select>
+          </div>
+
+          <div>
+            <label>Target Process Definition</label>
+            <br />
+            <select
+              onChange={(e) => {
+                migration_state.target.value = e.target.value;
+                add_query_params("target", migration_state.target.value);
+                engine_rest.process_definition.diagram(
+                  state,
+                  migration_state.target.value,
+                  migration_state.target_diagram,
+                );
+              }}
+            >
+              <option disabled selected>
+                -- Select target ---
+              </option>
+              <RequestState
+                state={state}
+                signal={list}
+                on_success={() =>
+                  list.value.data.map(({ id, definition }) => (
+                    <option
+                      key={id}
+                      disabled={migration_state.source.value == id}
+                      selected={migration_state.target.value == id}
+                      value={id}
+                    >
+                      {definition.name} – v{definition.version}
+                    </option>
+                  ))
+                }
+              />
+            </select>
+          </div>
+
+          <button
+            onClick={() =>
+              engine_rest.migration.generate(
+                state,
+                migration_state.source.value,
+                migration_state.target.value,
+              )
+            }
+            disabled={
+              migration_state.source.value === null ||
+              migration_state.target.value === null
+            }
+          >
+            Generate Mapping
+          </button>
+        </div>
+      </section>
 
       <h2>Mappings</h2>
-
-      <button
-        onClick={() =>
-          engine_rest.migration.generate(
-            state,
-            migration_state.source.value,
-            migration_state.target.value,
-          )
-        }
-      >
-        Generate Mapping
-      </button>
 
       <RequestState
         state={state}
@@ -300,16 +331,31 @@ const ProcessSelection = () => {
             state={migration_state}
             signal={migration_state.source_diagram}
             on_nothing={() => <p>Select source process defintion</p>}
-            on_success={() => (
-              <ReactBpmn
-                diagramXML={
-                  migration_state.source_diagram.value.data?.bpmn20Xml
-                }
-                onShown={() => console.log("shown")}
-                onLoading={() => console.log("loading")}
-                onError={() => console.log("error")}
-              />
-            )}
+            on_success={() => {
+              const bpmnModdle = new BpmnModdle();
+
+              // const { rootElement: definitionsA } = await;
+              bpmnModdle
+                .fromXML(migration_state.source_diagram.value.data?.bpmn20Xml)
+                .then(({ rootElement: definitions }) =>
+                  console.log(
+                    "diff moddle",
+                    definitions.rootElements.find(({ id }) => id === "invoice"),
+                    // .flowElements.filter(({ isImmediate }) => !isImmediate),
+                  ),
+                );
+
+              return (
+                <ReactBpmn
+                  diagramXML={
+                    migration_state.source_diagram.value.data?.bpmn20Xml
+                  }
+                  onShown={() => console.log("shown")}
+                  onLoading={() => console.log("loading")}
+                  onError={() => console.log("error")}
+                />
+              );
+            }}
           />
         </div>
         <div>
